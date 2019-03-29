77279c1f87913b93c727d597726aa36b3303ce8a
Teal <pershint@github.com>
2019-03-29 18:24:03 -0400

    Functions to write simple job submission scripts done and writing fine.

--- lib/script_writer.py
+++ lib/script_writer.py
@@ -1,21 +1,76 @@
-#Contains classes for writing bash scripts that are submitted on
-#The Fermilab Cluster
+#Functions used to write particular types of bash scripts used
+#in grid submission
 
+def WriteJobSubmission(fileloc,jobsubmitscript,configdict):
+    '''Write out the full TA script using the details given within
+    the input configuration file
+    Inputs: 
+          fileloc [string]
+          Specify the location to open this bash script at and ultimate save.
+          
+          jobsubmitscript [string]
+          Location of the script that will be submitted for running on the 
+          cluster.
 
-class BashScript(object):
-    def __init__(self,origfileloc=None):
-        if origfileloc is not None:
-            self.scriptfile = open(origfileloc,"w")
-        self.scriptloc = None
+          configdict [dictionary]
+          Configuration dictionary that contains details necessary for 
+          successful job submission.  See config/submit_default.json for
+          an example of a configuration dictionary.
+    '''
+    ourfile = open(fileloc,"w")
+    ourfile.write("source %s\n\n"%(configdict["GRIDSOURCE"]))
+    ourfile.write("setup jobsub_client\n\n")
+    subdetails = configdict["SUBMISSION_SPECS"]
 
-    def LoadBashScript(self,fileloc):
-        '''Given a file location, open the file and load it into the
-        class' scriptfile object for appending new stuff'''
-        self.scriptfile = open(fileloc,"a")
-        if not self.scriptfile.name.endswith("sh"):
-            print("WARNING: file being loaded does not end with sh...")
-
-    def SaveBashScript(self,fileloc):
-        '''Cloase the loaded scriptfile to finalize writing'''
+    #Write all flags that append to the job submission
+    flags = ""
+    newline = False
+    for key in subdetails:
+        if key == "resource-provides":
+            if len(subdetails[key])>0:
+                flags+="--%s=usage_model="%(key)
+            for resource in subdetails[key]:
+                flags+="%s,"%(resource)
+            flags.rstrip(",")
+            flags+=" "
+        elif key == "output_directory":
+            flags+="-d OUTPUT %s "%(subdetails[key])
+        elif key == "input_files":
+            if len(subdetails[key])>0:
+                for f in subdetails[key]:
+                    flags+="-f %s "%(f)
+        else:
+            flags+="--%s=%s "%(key,subdetails[key])
+        if newline:
+            flags+="\\\n"
+        newline = not newline
+    submitline = "jobsub_submit %s"%(flags)
+    submitline += "file://%s"%(jobsubmitscript)
+    ourfile.write(submitline)
+    ourfile.close()
     
+
+def WriteTAScript(fileloc,configdict):
+    '''Write out the full TA script using the details given within
+    the input configuration file.
+      Inputs:
+          fileloc [string]
+          Specify the location to open the bash script at and ultimate save.
+          
+          configdict [dictionary]
+          Configuration dictionary that contains details necessary for 
+          successful job submission.  See config/submit_default.json for
+          an example of a configuration dictionary.'''
+    ourfile = open(fileloc,"w")
+    ourfile.write("source %s\n\n"%(configdict["GRIDSOURCE"]))
+    comment1=("#Touch a dummy file in the working directory. \n"+
+              "#This is a hack that helps stalled jobs close faster\n\n")
+    ourfile.write(comment1)
+    dummyline=("DUMMY_OUTPUT_FILE=${CONDOR_DIR_OUTPUT}/${JOBSUBID}"+
+               "_${STEM}_dummy_output \n")
+    ourfile.write(dummyline)
+    ourfile.write("touch ${DUMMY_OUTPUT_FILE}\n")
+    ourfile.write("tar xfz config_tar.tar.gz\n")
+    ourfile.write("Analyse ./ToolChainConfig")
+    ourfile.close()
 
--- main.py
+++ main.py
@@ -5,31 +5,33 @@ import sys, os
 import shutil
 import lib.ArgParser as ap
 import lib.TextSweeper as ts
+import lib.script_writer as sw
 
 #Controls location to save output fit results
-basepath = os.path.dirname(__file__)
-configpath = os.path.abspath(os.path.join(basepath, "config"))
-outconfigpath = os.path.abspath(os.path.join(basepath,"TAConfig_out"))
-outscriptpath = os.path.abspath(os.path.join(basepath,"script_out"))
+BASEPATH = os.path.dirname(__file__)
+CONFIGPATH = os.path.abspath(os.path.join(BASEPATH, "config"))
+OUTCONFIGPATH = os.path.abspath(os.path.join(BASEPATH,"TAConfig_out"))
+OUTSCRIPTPATH = os.path.abspath(os.path.join(BASEPATH,"script_out"))
 
 if __name__=='__main__':
     print("WE BEGIN THE CONTROL")
     #Open the configuration dictionary
     with open(ap.args.CONFIG,"r") as f:
         ourconfig = json.load(f)
+
+    #Fill in the input/output files on the ToolAnalysis config template
     replacements = ourconfig["TA_REPLACEMENTS"]
     TASweeper = ts.TextSweeper(scandict=replacements)
     configtemp = ourconfig["TOOLANALYSISCONFIGNAME"]
-    TASweeper.ReplaceInDirectoryFiles("%s/%s"%(configpath,configtemp),
-                                      outconfigpath)
-    with tarfile.open("%s/config_tar.tar.gz"%(outconfigpath),"w:gz") as tar:
-        tar.add(outconfigpath, arcname=os.path.basename(outconfigpath))
+    TASweeper.ReplaceInDirectoryFiles("%s/%s"%(CONFIGPATH,configtemp),
+                                      OUTCONFIGPATH)
+    #Tar up the configs
+    with tarfile.open("%s/config_tar.tar.gz"%(OUTCONFIGPATH),"w:gz") as tar:
+        tar.add(OUTCONFIGPATH, arcname=os.path.basename(OUTCONFIGPATH))
     if ap.args.NOSAVE:
-        contents = glob.glob("%s/*"%(outconfigpath))
+        contents = glob.glob("%s/*"%(OUTCONFIGPATH))
         [os.remove(c) for c in contents]
-    #So, let's compartamentalize what we need to do here.
-    # - Need a class that writes the bash script which will be sent
-    #   to the cluster and used to run ToolAnalysis.
-    # - Have a class that writes a simple bash script which sources
-    #   the annie_setup, sets up the jobsub_client, then actually submits
-    #   the bash script described in the previous step
+
+    #Now, lets test our script writers
+    sw.WriteTAScript("./script_out/TAjobtest.sh",ourconfig)
+    sw.WriteJobSubmission("./script_out/submittest.sh","./TAjobtest.sh",ourconfig)
